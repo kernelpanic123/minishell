75f039159a4e0507242732595c337c23fed25c1b
kernelpanic <abdertouaitia@gmail.com>
2025-10-11 06:54:27 +0200

    ressource

---
+++ EXEC/pipes.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abtouait <abtouait@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/11 06:50:02 by abtouait          #+#    #+#             */
+/*   Updated: 2025/10/11 06:54:04 by abtouait         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+//pipe child parent etc
+//https://www.youtube.com/watch?v=NlFvGZoAgTs
+
+//fork function 
+//https://youtu.be/cex9XrZCU14?si=7SMM3HUvdRXVkpHq
